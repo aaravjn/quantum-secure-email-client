a31b229fd6b9bf0e47c4f05680b7740e49923ae7
Aarav <ajaaravbvg@gmail.com>
2023-11-07 17:04:16 +0530

    added .gitignore file to ignore compilation result

--- quant_sec_client/target/debug/deps/quant_sec_client-51f543d6b720ed31.d
+++
@@ -1,5 +0,0 @@
-/home/aarav/Desktop/quant-secure/quant_sec_client/target/debug/deps/quant_sec_client-51f543d6b720ed31: src/main.rs
-
-/home/aarav/Desktop/quant-secure/quant_sec_client/target/debug/deps/quant_sec_client-51f543d6b720ed31.d: src/main.rs
-
-src/main.rs:
--- quant_sec_client/target/debug/quant_sec_client.d
+++
@@ -1,1 +0,0 @@
-/home/aarav/Desktop/quant-secure/quant_sec_client/target/debug/quant_sec_client: /home/aarav/Desktop/quant-secure/quant_sec_client/src/main.rs
